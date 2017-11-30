78a597e2c348abe751178812367f260fde69edb6
Attila Domokos <adomokos@gmail.com>
2017-11-30 09:28:25 +0100

    Run the first SQL query

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
+{-#LANGUAGE TemplateHaskell #-}
+{-#LANGUAGE QuasiQuotes #-}
+
 module Main where
 
-import Lib
+import Database.YeshQL
+import Database.HDBC.MySQL
+
+[yesh|
+    -- name:countClientSQL :: (Int)
+    SELECT count(id) FROM clients;
+|]
+
+getConn :: IO Connection
+getConn = do
+    connectMySQL defaultMySQLConnectInfo {
+        mysqlHost     = "localhost",
+        mysqlDatabase = "hashmir_test",
+        mysqlUser     = "hashmir_user",
+        mysqlPassword = "shei7AnganeihaeF",
+        mysqlUnixSocket = "/tmp/mysql.sock"
+    }
+
+countClient :: IO ()
+countClient = do
+    conn <- getConn
+    Just (clientCount) <- countClientSQL conn
+    putStrLn $ "There are " ++ show clientCount ++ " records."
 
 main :: IO ()
-main = someFunc
+main = countClient
