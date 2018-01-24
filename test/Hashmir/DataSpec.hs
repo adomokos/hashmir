be20905a5ad5199c7b7b5e1eb4de8705a3656770
Attila Domokos <adomokos@gmail.com>
2018-01-24 08:47:26 -0600

    Verify the client record create logic

--- test/Hashmir/DataSpec.hs
+++ test/Hashmir/DataSpec.hs
@@ -1,12 +1,18 @@
 module Hashmir.DataSpec where
 
 import Test.Hspec
+import System.Process
+import qualified Hashmir.Data as D
 
 main :: IO ()
 main = hspec spec
 
+resetDB :: IO ()
+resetDB = callCommand "make build-db"
+
 spec :: Spec
-spec = do
+spec = before resetDB $ do
     describe "Hashmir Data" $ do
-        it "runs a test" $ do
-            True `shouldBe` True
+        it "creates a Client record" $ do
+            clientId <- D.insertClient "TestClient" "testclient"
+            clientId `shouldBe` 1
